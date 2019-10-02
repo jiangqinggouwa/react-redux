ac293070bd3b567da3d382968d7346f6d38e8b12
jiangll <jiangll@zjkeystone.com>
2019-10-02 21:35:42 +0800

    delete projece

--- PROJECT/src/reducers/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { memoList } from './memo';
-import { inputName, inputCount } from './input';
-
-export default combineReducers(
-  { memoList , inputName, inputCount} as any
-);
